d5ecc427116ab9bcb85a2294361ba046a845e60e
Sanmandresy <hei.sanda.3@gmail.com>
2022-08-31 15:25:39 +0300

    fix: boilerplae

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -6,13 +6,13 @@ const AboutUs: React.FC = () => {
     return(<>
         <section className={"w-full mt-8 mb-6"} id="about">
             <div className={"text-center uppercase text-2xl mb-3"} >À propos de nous</div>
-            <div className="max-w-sm">
+            <div className="max-w-sm mx-auto">
                 <Card imgSrc={fake_hotel}>
                     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Noteworthy technology acquisitions 2021
+                    Coding Pals Hotel
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                        Venez comme vous le sentez.
                     </p>
                 </Card>
             </div>
